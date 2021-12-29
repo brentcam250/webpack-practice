7812e9bf6f2660d9b731345d4b35ed5c0b8eb46e
Brent Cameron <bjcameron@gmail.com>
2021-12-29 09:56:49 -0700

    config webpack to load css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import myName from './myName';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,7 @@ function component() {
   // lodash now imported by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.innerHTML = myName('Brent');
+  element.classList.add('hello');
 
   return element;
 }
